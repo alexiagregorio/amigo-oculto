362c412a247074805aec569c0c63246d2b1fc6ea
Alexia Gregorio Dias <alexiagregorio6@hotmail.com>
2023-03-23 11:55:29 -0300

    att

--- teste.py
+++ teste.py
@@ -25,13 +25,3 @@ for i in range(len(amigos_secretos)):
 # Imprime na tela o nome do amigo secreto de cada participante
 for i in range(len(amigos_secretos)):
     print(participantes[i] + " tirou " + amigos_secretos[i] + " como amigo secreto!")
-    
-#Neste exemplo, o módulo random é usado para embaralhar a lista de participantes e sortear os amigos secretos.
-
-#Após embaralhar a lista de amigos secretos, um loop for é usado para garantir que ninguém tire a si próprio e que não haja amigos secretos repetidos.
-
-#Para verificar se um participante tirou a si próprio, o loop verifica se o nome do amigo secreto correspondente é igual ao nome do participante na posição atual do loop. Se for esse o caso, o loop inteiro é reiniciado a partir do início.
-
-#Para verificar se um amigo secreto já foi sorteado anteriormente, o loop usa o método index para procurar o índice do amigo secreto atual na lista de amigos secretos. Se esse índice for menor que a posição atual do loop, significa que o amigo secreto já foi sorteado anteriormente e o loop inteiro é reiniciado.
-
-#Finalmente, outro loop for é usado para imprimir o nome do amigo secreto correspondente a cada participante na tela.
